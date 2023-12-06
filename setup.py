edf0702b8cda14588ff1eca1bb366abde6511909
jhn-nt <angelotti.giovanni.ds@gmail.com>
2023-12-06 11:41:49 +0000

    fix

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name="mimiciv__fluid_balance",
-    version="0.1",
+    version="0.2",
     author="G. Angelotti, MSc",
     packages=["mimiciv__fluid_balance"],
+    include_package_data=True,
     install_requires=["google-cloud-bigquery==3.13.0"]
 )
